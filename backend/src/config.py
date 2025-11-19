06bb332d91510334d7a37b21174345c66673771c
AlphaJugger11 <alihussain11242004@example.com>
2025-11-19 09:17:55 +0500

    Changed dataset path

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,12 +1,12 @@
 import os
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-DATASET_DIR = BASE_DIR / "dataset"
-
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent.parent
+DATASET_DIR = BASE_DIR / "Hack the Track" / "dataset"
+print(BASE_DIR)
 API_HOST = "0.0.0.0"
 API_PORT = 8000
-
+print(os.listdir(DATASET_DIR))
 CACHE_MAX_SIZE_MB = 500  # Optimized for 8GB RAM systems
 
 TRACKS = [
